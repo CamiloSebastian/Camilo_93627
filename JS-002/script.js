a523a32635b21ed5f6d0f82d3ea456c9824f9c6e
Camilo Sebastian Lopes Miranda <camilosebastianlopesmiranda@gmail.com>
2025-10-10 13:27:48 +0000

    Calculadora certa

--- JS-002/script.js
+++ JS-002/script.js
@@ -1,22 +1,21 @@
 document.getElementById('calculatorForm').addEventListener
 ('submit',
      function(event) {
-                         event.preventDefault();
+                        event.preventDefault();
 
-    // Pegando os valores dos campos
-    const num1 = parsefloat(document.getElementById('num1').value);
-    const num2 = parsefloat(document.getElementById('num2').value);
-    
-    //validar se os números são válidos
-    if (IsNah(num1) || isNah(num2)) {
-        alert('Por favor, insira números válidos!');
-        return;
-    }
+                        // Pegando os valores dos campos
+                        const num1 = parseFloat(document.getElementById('num1').value);
+                        const num2 = parseFloat(document.getElementById('num2').value);
 
-    // Realizando a soma
-    const result = num1 + num2;
+                        // Validando se os números são válidos
+                        if (isNaN(num1) || isNaN(num2)) {
+                            alert('Por favor, insira números válidos!');
+                            return;
+                        }
 
-    //exibindo o resultado
-    document.getElementById('result').textContent = result;
-    }
-)
+                        // Realizando a soma
+                        const result = num1 + num2;
+
+                        // Exibindo o resultado
+                        document.getElementById('result').textContent = result;
+});
