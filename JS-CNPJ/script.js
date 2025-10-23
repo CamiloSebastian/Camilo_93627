1e6939cfc8bbccac8b816124d1184f6c3dff64be
Camilo Sebastian Lopes Miranda <camilosebastianlopesmiranda@gmail.com>
2025-10-23 16:38:57 -0300

    tentativa de cnpj

--- JS-CNPJ/script.js
+++ JS-CNPJ/script.js
@@ -1,15 +1,15 @@
 // Função que será chamada ao clicar no botão
 function consultarCep() {
     // Obtém o valor do campo CEP
-    const cep = document.getElementById('cep').value;
+    const cpnj = document.getElementById('cnpj').value;
 
-    // Verifica se o CEP tem 8 dígitos
-    if (cep.length !== 14) {
-        alert("Por favor, insira um Cnpj válido com 8 dígitos.");
+    // Verifica se o CNPJ tem 18 dígitos
+    if (cnpj.length !== 18) {
+        alert("Por favor, insira um Cnpj válido com 18 dígitos.");
         return; // Interrompe a execução da função se o CEP for inválido
     }
 
-    // URL da API de CEP (usando o serviço ViaCEP como exemplo)
+    // URL da API de CPNJ (usando o serviço ViaCEP como exemplo)
     const url = `https://brasilapi.com.br/api/cnpj/v1/{cnpj}`;
 
     // Faz uma requisição à API para obter os dados do CEP
@@ -18,18 +18,18 @@ function consultarCep() {
         .then(data => {
             // Verifica se o CEP foi encontrado
             if (data.erro) {
-                alert("CEP não encontrado.");
+                alert("CNPJ não encontrado.");
                 return; // Interrompe a execução se o CEP não for válido
             }
 
             // Atualiza os campos no formulário com os dados retornados pela API
-            document.getElementById('rua').textContent = data.logradouro;
-            document.getElementById('bairro').textContent = data.bairro;
-            document.getElementById('cidade').textContent = data.localidade;
-            document.getElementById('estado').textContent = data.uf;
+            document.getElementById('pais').textContent = data.logradouro;
+            document.getElementById('email').textContent = data.bairro;
+            document.getElementById('bairro').textContent = data.localidade;
+            document.getElementById('situacao').textContent = data.uf;
         })
         .catch(error => {
-            console.error("Erro ao consultar o CEP:", error); // Loga erros no console
-            alert("Ocorreu um erro ao consultar o CEP.");
+            console.error("Erro ao consultar o CNPJ:", error); // Loga erros no console
+            alert("Ocorreu um erro ao consultar o CNPJ.");
         });
 }
